93c913d98685e1c3f0cf29432e3469e8f6ead1a9
Vinokurov Kirill <vin.kirill@gmail.com>
2017-02-08 17:13:01 +0400

    i2c

--- include/abstract/i2c.h
+++ include/abstract/i2c.h
@@ -27,24 +27,29 @@
 void i2c_init_master(uint8_t speed, bool allow_broadcast = false, uint8_t address = 0x00)
 {
 	TWBR = speed;
-	TWSR &= ~0x03;
-	TWAR = (address << 1) | (allow_broadcast);
-	TWCR =  (1 << TWEN) |			// enable twi
-			(0 << TWIE);			// disable interrupts
+	TWSR = 0;
+//	TWAR = (address << 1) | (allow_broadcast);
+//	TWCR =  (1 << TWEN) |			// enable twi
+//			(0 << TWIE);			// disable interrupts
 }
 						
 
 void i2c_start(uint8_t *error = 0)
 {
+//	TWCR = (1<<TWINT)|(1<<TWSTA)|(1<<TWEN);
+//	uart_send_str("Control register set\n");
+//	i2c_waitFlag;
+//	uart_send_str("Flag received!\n");
+//	uint8_t status = TWSR & I2C_STATUS_MASK;
+//    if (!((status & I2C_STATUS_START) || (status & I2C_STATUS_REPSTART)))
+//	{
+//		if (error) *error = status;
+//    }
+
+	uart_send_str("Control register set!\n");
 	TWCR = (1<<TWINT)|(1<<TWSTA)|(1<<TWEN);
-	uart_send_str("Control register set\n");
-	i2c_waitFlag;
+    while(!(TWCR & (1<<TWINT)));
 	uart_send_str("Flag received!\n");
-	uint8_t status = TWSR & I2C_STATUS_MASK;
-    if (!((status & I2C_STATUS_START) || (status & I2C_STATUS_REPSTART)))
-	{
-		if (error) *error = status;
-    }
 }
 
 void i2c_write_byte(uint8_t data)
